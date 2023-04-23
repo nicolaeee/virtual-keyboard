fb5be3451153dd53ea06937e08bde0b161685675
onyxgeek <onyxpeek@gmail.com>
2023-04-23 21:01:22 +0300

    feat: add first layout of the keyboard

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import { test } from "./test";
 import './style.scss';
 
-document.body.innerHTML = 'WEBBssssssфыафыаBssdBBPACKK'
-test();
+const parentDiv = document.createElement('div');
+const firstChildDiv = document.createElement('div');
+const secondChildDiv = document.createElement('div');
+
+firstChildDiv.textContent = 'This is the first child div';
+secondChildDiv.textContent = 'This is the second child div';
+
+parentDiv.appendChild(firstChildDiv);
+parentDiv.appendChild(secondChildDiv);
+
+document.body.appendChild(parentDiv);
