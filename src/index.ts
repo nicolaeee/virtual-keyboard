6b28c6740b19ba507fc846bd79e98c915befb68c
onyxgeek <onyxpeek@gmail.com>
2023-04-25 16:14:37 +0300

    feat: add the very first design of the keyboard

--- src/index.ts
+++ src/index.ts
@@ -19,49 +19,48 @@ parentDiv.appendChild(secondChildDiv);
 document.body.appendChild(parentDiv);
 
 
-// Define the rows of keys for the keyboard
-const keyRows = [
-    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='],
-    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
-    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
-    ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'],
-    ['Ctrl', 'Alt', ' ', 'Alt', 'Ctrl']
-  ];
+const keyValues = [
+  ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace"],
+  ["Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\"],
+  ["Caps Lock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter"],
+  ["Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "▲", "Shift"],
+  ["Ctrl", "Win", "Alt", " ", "Alt", "Ctrl", "◄", "▼", "►"]
+];
 
-  // Create the keyboard container
-  const keyboardContainer = document.createElement('div');
-  keyboardContainer.classList.add('keyboard-container');
+// Create the keyboard container
+const keyboardContainer = document.createElement('div');
+keyboardContainer.classList.add('keyboard-container');
 
-  // Loop through the rows of keys and create buttons for each key
-  for (let i = 0; i < keyRows.length; i++) {
-    const keyRow = keyRows[i];
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('key-row');
+// Loop through the rows of keys and create buttons for each key
+for (let i = 0; i < keyValues.length; i++) {
+  const keyRow = keyValues[i];
+  const rowDiv = document.createElement('div');
+  rowDiv.classList.add('key-row');
 
-    for (let j = 0; j < keyRow.length; j++) {
-      const key = keyRow[j];
-      const keyButton = document.createElement('button');
-      keyButton.classList.add('key');
-      keyButton.textContent = key;
+  for (let j = 0; j < keyRow.length; j++) {
+    const key = keyRow[j];
+    const keyButton = document.createElement('button');
+    keyButton.classList.add('key');
+    keyButton.textContent = key;
 
-      // Add event listener for each key button
-      keyButton.addEventListener('click', () => {
-        inputField.focus();
-        inputField.dispatchEvent(new KeyboardEvent('keydown', {'key': key}));
-      });
+    // Add event listener for each key button
+    keyButton.addEventListener('click', () => {
+      inputField.focus();
+      inputField.dispatchEvent(new KeyboardEvent('keydown', {'key': key}));
+    });
 
-      rowDiv.appendChild(keyButton);
-    }
-
-    keyboardContainer.appendChild(rowDiv);
+    rowDiv.appendChild(keyButton);
   }
 
-  // Create the input field
-  const inputField = document.createElement('textarea');
-  inputField.classList.add('input-field');
+  keyboardContainer.appendChild(rowDiv);
+}
 
-  // Append the input field to the firstChildDiv
-  firstChildDiv.appendChild(inputField);
+// Create the input field
+const inputField = document.createElement('textarea');
+inputField.classList.add('input-field');
 
-  // Append the keyboard container to the second child div
-  secondChildDiv.appendChild(keyboardContainer);
+// Append the input field to the firstChildDiv
+firstChildDiv.appendChild(inputField);
+
+// Append the keyboard container to the second child div
+secondChildDiv.appendChild(keyboardContainer);
