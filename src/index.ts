7b24fc6c49d0e7124bd89e10ff8474575104e5a1
onyxgeek <onyxpeek@gmail.com>
2023-04-26 11:28:04 +0300

    feat: add the basic style to the keyboard

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ document.body.appendChild(parentDiv);
 // Define the rows of keys for the keyboard
 const keyRows = [
   ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace"],
-  ["Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "Del"],
+  ["Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "Del"],
   ["Caps Lock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter"],
   ["Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "▲", "Shift"],
   ["Ctrl", "Win", "Alt", " ", "Alt", "Ctrl", "◄", "▼", "►"]
